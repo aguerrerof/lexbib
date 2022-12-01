1fed48f35b9cd6fc1c435264b100ea4ef358c991
Andrés Guerrero <afguerrerof@gmail.com>
2022-12-01 18:05:22 -0500

    Adding prefix for admin routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')
+    ->group(function () {
+        Auth::routes([
+            'register' => false,
+            'reset' => true,
+            'verify' => true,
+        ]);
+    });
