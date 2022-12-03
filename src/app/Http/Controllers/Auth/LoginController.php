445c153995c1ac890b3a2b040d8bd8cfdbbd434a
Andrés Guerrero <afguerrerof@gmail.com>
2022-12-02 19:41:19 -0500

    Adding theme

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,27 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse|\Illuminate\Http\RedirectResponse|mixed
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect()->route('login');
     }
 }
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
     <!-- DataTable -->
     <script type="text/javascript" src="{{{ URL::asset('js/jquery.min.js')}}}"></script>
@@ -25,69 +25,106 @@
 
     <link href="{{ asset('assets/DataTables/css/jquery.dataTables.min.css') }}" rel="stylesheet">
 
+    <!--Templates -->
+    <link href="{{ asset('assets/img/favicon.png') }}" rel="stylesheet">
+    <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+
 </head>
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                {{ config('app.name', 'Laravel') }}
+<!-- ======= Header ======= -->
+@auth
+    <header id="header" class="header fixed-top d-flex align-items-center">
+
+        <div class="d-flex align-items-center justify-content-between">
+            <a href="{{ route("home") }}" class="logo d-flex align-items-center">
+                <span class="d-none d-lg-block"> {{ config('app.name', 'Laravel') }}</span>
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ms-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        @if (Route::has('login'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @endif
-
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ ucwords(Auth::user()->name) }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
+            <i class="bi bi-list toggle-sidebar-btn"></i>
         </div>
-    </nav>
+        <nav class="header-nav ms-auto">
+            <!-- Authentication Links -->
 
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
+            <ul class="d-flex align-items-center">
+
+                <li class="nav-item dropdown pe-3">
+
+                    <span class="d-none d-md-block ps-2"> Usuario:&nbsp;{{ ucwords(Auth::user()->name) }}</span>
+                </li><!-- End Profile Nav -->
+
+            </ul>
+
+        </nav>
+
+    </header><!-- End Header -->
+@endauth
+@auth
+    <!-- ======= Sidebar ======= -->
+    <aside id="sidebar" class="sidebar">
+
+        <ul class="sidebar-nav" id="sidebar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route("tags.index") }}">
+                    <i class="bi bi-grid"></i>
+                    <span>Tags</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                   document.getElementById('logout-form').submit()">
+                    <i class="bi bi-door-closed"></i> {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
+        </ul>
+    </aside><!-- End Sidebar-->
+@endauth
+<main id="main" class="main">
+    <section class="section dashboard">
+        <div class="row">
+            @yield('content')
+        </div>
+    </section>
+
+</main>
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Alexander</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        Designed by <a href="https://lanistek.com">Lanistek</a>
+    </div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="{{{ URL::asset('assets/vendor/apexcharts/apexcharts.min.js')}}}"></script>
+<script src="{{{ URL::asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}}"></script>
+<script src="{{{ URL::asset('assets/vendor/chart.js/chart.min.js')}}}"></script>
+<script src="{{{ URL::asset('assets/vendor/echarts/echarts.min.js')}}}"></script>
+<script src="{{{ URL::asset('assets/vendor/quill/quill.min.js')}}}"></script>
+<script src="{{{ URL::asset('assets/vendor/simple-datatables/simple-datatables.js')}}}"></script>
+<script src="{{{ URL::asset('assets/vendor/tinymce/tinymce.min.js')}}}"></script>
+<script src="{{{ URL::asset('js/main.js')}}}"></script>
 </body>
 </html>
