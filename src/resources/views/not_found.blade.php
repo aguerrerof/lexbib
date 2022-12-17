43833f5b8d24fdfb4e5da843b21a5f033393abd0
Andrés Guerrero <afguerrerof@gmail.com>
2022-12-17 15:07:36 -0500

    adding custom not found page

---
+++ src/resources/views/not_found.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>404</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <!-- Styles -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!--Templates -->
+    <link href="{{ asset('assets/img/favicon.png') }}" rel="stylesheet">
+    <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+<main>
+    <div class="container">
+
+        <section class="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+            <h1>404</h1>
+            <h2>No pudimos encontrar la página que estabas buscando</h2>
+            <a href="{{ route("landing") }}" class="btn btn-outline-secondary" role="button"
+               aria-disabled="true"><i class="bx bx-rotate-left"></i>Regresar</a>
+            <img src="assets/img/not-found.svg" class="img-fluid py-5" alt="Page Not Found">
+            <div class="credits">
+                Created by <a href="https://lanistek.com" target="_blank">Lanistek</a>
+            </div>
+        </section>
+
+    </div>
+</main><!-- End #main -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+</body>
+
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -19,7 +19,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('landing');
+
+Route::get('/404', function () {
+    return view('not_found');
+})->name('not-found');
 
 
 Route::prefix('admin')
