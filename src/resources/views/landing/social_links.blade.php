0d4b111448dfe504568f6efea3a2a64e43e7396b
Andrés Guerrero <afguerrerof@gmail.com>
2023-04-07 14:27:17 -0500

    Update social_links.blade.php

--- src/resources/views/landing/social_links.blade.php
+++ src/resources/views/landing/social_links.blade.php
@@ -1,27 +1,57 @@
 <div class="pt-6 d-flex flex-column align-items-center">
-    <div class="social-links mt-2">
-        <div class="col-lg-12">
+    <div class="social-links mt-1">
+        <div class="container p-1 pb-0">
+            <section class="mb-1">
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #3b5998;"
+                    href="{{$socialLinks['facebook']}}"
+                    target="_blank"
+                    role="button"
+                ><i class="fab fa-facebook-f"></i>
+                </a>
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #55acee;"
+                    href="{{$socialLinks['twitter']}}"
+                    target="_blank"
+                    role="button"
+                ><i class="fab fa-twitter"></i>
+                </a>
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #0082ca;"
+                    href="{{$socialLinks['linkedin']}}"
+                    target="_blank"
+                    role="button">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #0082ca;"
+                    href="{{$socialLinks['telegram']}}"
+                    target="_blank"
+                    role="button">
+                    <i class="fab fa-telegram"></i>
+                </a>
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #0082ca;"
+                    href="{{$socialLinks['skype']}}"
+                    target="_blank"
+                    role="button">
+                    <i class="fab fa-skype"></i>
+                </a>
 
-            <a class="btn btn-outline-info" href="{{$socialLinks['twitter']}}" target="_blank">
-                <span><i class="fa-brands fa-twitter"></i>&nbsp;</span><span>Compartir por Twitter</span>
-            </a>
-            <a class="btn btn-outline-primary" href="{{$socialLinks['facebook']}}" target="_blank">
-                <span><i class="fa-brands fa-facebook"></i>&nbsp;</span><span>Compartir por Facebook</span>
-            </a>
-            <a class="btn btn-outline-primary" href="{{$socialLinks['linkedin']}}" target="_blank">
-                <span> <i class="fa-brands fa-linkedin"></i></span>&nbsp;<span>Compartir por Linkedin</span>
-            </a>
-        </div>
-        <div class="col-lg-12">
-            <a class="btn btn-outline-primary" href="{{$socialLinks['telegram']}}" target="_blank">
-                <span> <i class="fa-brands fa-telegram"></i></span>&nbsp;<span>Compartir por Telegram</span>
-            </a>
-            <a class="btn btn-outline-primary" href="{{$socialLinks['skype']}}" target="_blank">
-                <span> <i class="fa-brands fa-skype"></i></span>&nbsp;<span>Compartir por Skype</span>
-            </a>
-            <a class="btn btn-outline-success" href="{{$socialLinks['whatsapp']}}" target="_blank">
-                <span> <i class="fa-brands fa-whatsapp"></i></span>&nbsp;<span>Compartir por Whatsapp</span>
-            </a>
+                <a
+                    class="btn text-white btn-floating m-1"
+                    style="background-color: #25d366;"
+                    href="{{$socialLinks['whatsapp']}}"
+                    target="_blank"
+                    role="button">
+                    <i class="fab fa-whatsapp fa-1x"></i>
+                </a>
+            </section>
         </div>
     </div>
 </div>
